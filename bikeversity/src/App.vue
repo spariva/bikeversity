ba0de8cc4fcf0155e22602686fcb23faff09993c
Maki Spariva <makspariva@gmail.com>
2024-12-11 14:23:49 +0100

    Menu y colors

--- bikeversity/src/App.vue
+++ bikeversity/src/App.vue
@@ -17,5 +17,5 @@ export default {
 </script>
 
 <style>
-@import './styles/style.css';
+@import 'assets/styles/style.css';
 </style>
--- bikeversity/src/components/MenuComponent.vue
+++ bikeversity/src/components/MenuComponent.vue
@@ -1,5 +1,5 @@
 <template>
-        <nav class="navbar navbar-expand-lg mb-5" style="background-color:#ffd9ec">
+        <nav class="navbar navbar-expand-lg mb-5" style="background-color:#638ec1">
             <div class="container-fluid">
               <a class="navbar-brand" href="{% url 'recipes:recipe_list' %}"><img class="navbar-image" src="{% static 'recipes/images/general/onion_purple.jpg' %}" alt="VeggieVibe" aria-current="page" id="home-icon">VeggieVibe</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -8,12 +8,6 @@
               <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                   <a class="nav-link active" aria-current="page" href="{% url 'recipes:recipe_list' %}">Home</a>
-                    <a class="nav-link" href="{% url 'users:profile' %}">My account</a>
-                    <form method="post" action="{% url 'logout' %}">
-                        <button class="nav-link" type="submit">Log-out</button>
-                    </form>
-                    <a class="nav-link" href="{% url 'users:profile' %}">Log-in</a>
-                    <a class="nav-link" href="{% url 'users:signup' %}">Sign-up</a>
                   <a class="nav-link" href="#">API</a>
                   <a class="nav-link" href="https://github.com/spariva/VeggieVibe/issues" target="_blank">Support</a>
                   <a class="nav-link" target="_blank" href="https://github.com/spariva">Developer</a>
